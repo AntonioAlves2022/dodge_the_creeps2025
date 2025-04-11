e88a8f11e560fdf2621c5d7aa40d8c8a70ffd2e0
Antonio Alves <antonio.alves@duque.g12.br>
2025-04-11 15:54:45 -0300

    Foi criada a cena principal do jogo

--- scripts/mob.gd
+++ scripts/mob.gd
@@ -1,1 +1,9 @@
 extends RigidBody2D
+
+func _ready() -> void:
+	var mob_types = Array($sprite.sprite_frames.get_animation_names())
+	$sprite.animation = mob_types.pick_random()
+	
+	
+func _on_notifier_screen_exited() -> void:
+	queue_free()
